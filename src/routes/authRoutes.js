c265208434c8aca3d4f5bc7bc4fc13c32f194179
Max <maxchergik@gmail.com>
2025-08-04 13:20:44 +0300

    first commit

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,70 @@
+import express from 'express'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import db from '../db.js'
+
+const router = express.Router();
+
+// register a new user endpoint /auth/register
+router.post('/register', (req, res) => {
+    const { username, password } = req.body;
+
+    const hashedPassword = bcrypt.hashSync(password, 8);
+
+    try {
+        const insertUser = db.prepare(`
+                INSERT INTO users(username, password)
+                VALUES (?, ?)
+            `);
+        const result = insertUser.run(username, hashedPassword);
+
+        // add default todo
+        const defaultTodo = `Hello, ${username}! Add your first todo...`;
+        const insertTodo = db.prepare(`
+                INSERT INTO todos (user_id, task)
+                VALUES (?, ?)
+            `);
+        insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+        // create a token
+        const token = jwt.sign({ id: result.lastInsertRowid }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+        res.json({ token })
+
+        res.sendStatus(201);
+    } catch (err) {
+        console.log(err.message);
+        res.sendStatus(503);
+    }
+})
+
+router.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const getUser = db.prepare(`
+            SELECT * FROM users WHERE username = ?
+        `);
+        const user = getUser.get(username);
+
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+        if (!passwordIsValid) {
+            return res.status(401).send({ message: 'Invalid password' });
+        }
+
+        // login successful, create a token
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+        res.json({ token }).sendStatus(200);
+
+    } catch (err) {
+        console.log(err.message);
+        res.sendStatus(503);
+    }
+})
+
+export default router;
